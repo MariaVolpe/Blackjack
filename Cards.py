69f9f2014449cbc52f8424195ae6e5493874dbed
Maria Volpe <mariaCHVolpe@gmail.com>
2017-12-17 22:25:11 -0500

    Add constructors for all classes

--- Cards.py
+++ Cards.py
@@ -3,3 +3,7 @@ class Cards:
     suit = 0
     rank = 0
     in_deck = True
+
+    def __init__(suit, rank):
+        self.suit = suit
+        self.rank = rank
--- Deck.py
+++ Deck.py
@@ -5,5 +5,22 @@ class Deck:
     cards_in_deck=[]
     cards_on_table=[]
     players=[]
-    CPU_dealer = Players.Players("Dealer")
+    CPU_dealer = Players.Players('Dealer')
 
+    #constructor
+    #initializes 52 cards in the deck in use
+    def __init__():
+        rank_count = 1
+        suit_count = 1
+
+        for i in range(52):
+            if rank_count >= 14
+                rank_count = 1
+            if suit_count >= 4
+                suit_count = 1
+
+            x = Cards.Cards(suit_count, rank_count)
+            cards_in_deck.append(x)
+
+            rank_count += rank_count
+            suit_count += suit_count
